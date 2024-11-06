fix: backpropagation

models/VNet_v1.py
@@ -163,11 +163,15 @@ class softmax_out(nn.Module):
         # Do NOT add normalize layer, or its values vanish.
         y_conv = self.conv_2(self.conv_1(x))
         
-        # y_conv = y_conv.permute(0, 2, 3, 4, 1).contiguous()
-        # y_conv = y_conv.view(y_conv.size(0), y_conv.numel() // (2 * y_conv.size(0)), 2)
-        y_conv = F.softmax(y_conv,dim=1)
-
-        return y_conv
+        """ Hacer return de dos canales hace que luego tengamos que usar funciones no diferenciables
+        , por lo que el grafo de gradiantes se rompe... Mejor usar una softmax o en este caso
+        un sigmoide... Sigmoid es lo mismo que softmax pero solamente para una clase. """
+        # # y_conv = y_conv.permute(0, 2, 3, 4, 1).contiguous()
+        # # y_conv = y_conv.view(y_conv.size(0), y_conv.numel() // (2 * y_conv.size(0)), 2)
+        # y_conv = F.softmax(y_conv,dim=1)
+        # return y_conv
+    
+        return (nn.Sigmoid()(y_conv)).squeeze(1)
         """
             Martin: Sigmoid solo saca un mapa de probabilidades que se supone que es la probilidad
             en este caso de tener cancer. Pero la red de entrenamiento espera encontrar dos mapas
@@ -215,7 +219,7 @@ class VNet(nn.Module):
         self.deconv_2 = deconv3d_x2(128, 64)
         self.deconv_1 = deconv3d_x1(64, 32)
 
-        self.out = softmax_out(32, 2)
+        self.out = softmax_out(32, 1)
 
     def forward(self, x):
         conv_1 = self.conv_1(x)

train_v2.py
@@ -9,9 +9,11 @@ import torch.optim as optim
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
-# from logger import Logger as logger
+# from logger.logger import Logger as logger 
 from evaluate.loss.dice_loss import DiceLoss
 from evaluate.loss.dice_loss import dsc
+# from torch.utils.tensorboard import SummaryWriter
+
 
 import torchvision.transforms as transforms_tv
 import utils.prepare.promise12 as promise12
@@ -21,7 +23,12 @@ import models.VNet_v1 as VNet_v1
 
 def main(args):
     makedirs(args)
-    snapshotargs(args)
+    # logger = Logger("./logs/loss_logs_v2.json")
+    # snapshotargs(args)
+    
+    # writer = SummaryWriter()
+
+    
     device = torch.device("cpu" if not torch.cuda.is_available() else args.device)
 
     loader_train, loader_valid = data_loaders(args)
@@ -34,8 +41,10 @@ def main(args):
     best_validation_dsc = 0.0
 
     optimizer = optim.Adam(vnet.parameters(), lr= args.lr)
+    
+    trainF = open(os.path.join("./results/logs/", 'train_2.csv'), 'w')
+    validF = open(os.path.join("./results/logs/", 'validation_2.csv'), 'w')
 
-    # logger = Logger(args.logs)
     loss_train = []
     loss_valid = []
 
@@ -65,21 +74,15 @@ def main(args):
                     x = x.requires_grad_(True) 
 
                     y_pred = vnet(x)
-
-                    _, y_pred = y_pred.max(1)
                     
+                    loss = dsc_loss(y_pred, y_true)
+                    
+                    # y_pred = torch.argmax(y_pred, dim=1)  # rompe el grafo de gradiantes...
+                    # _, y_pred = y_pred.max(1) # rompe el grafo de gradiante...
                     # y_true = y_true.to(dtype=torch.long)
-                    
                     # loss_function = nn.CrossEntropyLoss(reduction='mean')
                     # loss = loss_function(y_pred, y_true)
                     
-                    # print("\nTRAIN --> CE Loss per batch " + str(loss.item()))
-
-                    loss = dsc_loss(y_pred, y_true)
-                    
-                    if phase == "train":
-                        loss = loss.requires_grad_(True)
-
                     if phase == "valid":
                         loss_valid.append(loss.item())
                         y_pred_np = y_pred.detach().cpu().numpy()
@@ -90,16 +93,16 @@ def main(args):
                         validation_true.extend(
                             [y_true_np[s] for s in range(y_true_np.shape[0])]
                         )
-                        if (epoch % args.vis_freq == 0) or (epoch == args.epochs - 1):
-                            if i * args.batch_size < args.vis_images:
-                                tag = "image/{}".format(i)
-                                num_images = args.vis_images - i * args.batch_size
-                                # logger.image_list_summary(
-                                #     tag,
-                                #     log_images(x, y_true, y_pred)[:num_images],
-                                #     step,
-                                # )
-                                # print("\nVALIDATION --> tag " + str(tag) + "\tnum_images " + str(num_images))
+                        
+                        # if (epoch % args.vis_freq == 0) or (epoch == args.epochs - 1):
+                        #     if i * args.batch_size < args.vis_images:
+                        #         tag = "image/{}".format(i)
+                        #         num_images = args.vis_images - i * args.batch_size
+                        #         # logger.image_list_summary(
+                        #         #     tag,
+                        #         #     log_images(x, y_true, y_pred)[:num_images],
+                        #         #     step,
+                        #         # )
 
                     if phase == "train":
                         loss_train.append(loss.item())
@@ -107,10 +110,21 @@ def main(args):
                         optimizer.step()
 
                 if phase == "train" and (step + 1) % 10 == 0:
+                    trainF.write('{},{}\n'.format(epoch, np.mean(loss_train)))
+                    trainF.flush()
+                    # if len(loss_train) > 0:
+                    #     writer.add_scalar("Loss/train", np.mean(loss_train), epoch)
                     # log_loss_summary(logger, loss_train, step)
                     loss_train = []
 
             if phase == "valid":
+                
+                validF.write('{},{}\n'.format(epoch, np.mean(loss_valid)))
+                validF.flush()
+                
+                # if len(loss_valid) > 0:
+                #     writer.add_scalar("Loss/valid", np.mean(loss_valid), epoch)
+
                 # log_loss_summary(logger, loss_valid, step, prefix="val_")
                              
                 # mean_dsc = np.mean(
@@ -127,13 +141,17 @@ def main(args):
                         validation_true
                     )
                 )
+                
                 # logger.scalar_summary("val_dsc", mean_dsc, step)
+                
                 if mean_dsc > best_validation_dsc:
                     best_validation_dsc = mean_dsc
                     torch.save(vnet.state_dict(), os.path.join(args.weights, "vnet.pt"))
                 loss_valid = []
 
     print("Best validation mean DSC: {:4f}".format(best_validation_dsc))
+    trainF.close()
+    validF.close()
 
 
 def data_loaders(args):